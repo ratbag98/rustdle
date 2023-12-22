9869672f435cfc43ae7cae91e44b2f13a69d113b
Rob Rainthorpe <robr@robrainthorpe.com>
2023-12-22 11:53:10 -0800

    bad unique letter function

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,5 +1,6 @@
 //! A Squaredle puzzle
 use num_integer::{Integer, Roots};
+use std::collections::BTreeSet;
 
 /// is a grid of letters "square" (l x l)?
 fn square<T: Integer + Roots + Copy>(size: T) -> bool {
@@ -29,5 +30,17 @@ impl Grid {
     /// retrieve all the letters from the Grid
     pub fn letters(self) -> String {
         self.grid_letters.into_iter().collect()
+    }
+
+    /// compose set of unique letters in grid
+    pub fn unique_letters(self) -> String {
+        // want the letters sorted so BTree rather than Hash
+        let mut unique_letters: BTreeSet<char> = BTreeSet::new();
+        for l in self.grid_letters {
+            unique_letters.insert(l);
+        }
+        let result = unique_letters.into_iter().collect();
+        println!("Unique returned {}", result);
+        result
     }
 }
--- tests/puzzle.rs
+++ tests/puzzle.rs
@@ -24,4 +24,10 @@ mod puzzle_tests {
             )),
         }
     }
+
+    #[test]
+    fn retrieve_unique_letters_from_grid() {
+        let good_grid = Grid::new("VALIDGRID").expect("Invalid letter selection");
+        assert!(good_grid.unique_letters() == "ADGILRV");
+    }
 }
